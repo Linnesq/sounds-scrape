8494c87d920c4323ad6c5b5fcbf173f2e9fa5cc8
Richard Gilmour <rgilmour.mail@gmail.com>
2021-10-19 00:45:03 +0100

    test console.log hit

--- app/utils/logger.test.js
+++ app/utils/logger.test.js
@@ -7,12 +7,16 @@ const { report } = require("./logger");
 describe("logger", () => {
   describe("report", () => {
     it("logs to a file", () => {
+      const spy = jest.spyOn(console, 'log');
+
       report("something");
+
       expect(fs.appendFileSync).toHaveBeenCalledTimes(1);
       expect(fs.appendFileSync).toHaveBeenCalledWith(
         expect.anything(),
         expect.stringContaining("something")
       );
+      expect(spy).toHaveBeenCalledWith(expect.stringContaining("something"))
     });
   });
 });
