60870a86fae5f11392a6bb605ce58265ded2bf46
Richard Gilmour <rgilmour.mail@gmail.com>
2021-10-19 00:50:12 +0100

    Improve config (#3)
    * Allows multiple shows in config
    * Add coverage checks for CI, with additional tests

---
+++ app/utils/logger.test.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+const { isMainThread } = require("worker_threads");
+jest.mock("fs");
+
+const { report } = require("./logger");
+
+describe("logger", () => {
+  describe("report", () => {
+    it("logs to a file", () => {
+      const spy = jest.spyOn(console, "log");
+
+      report("something");
+
+      expect(fs.appendFileSync).toHaveBeenCalledTimes(1);
+      expect(fs.appendFileSync).toHaveBeenCalledWith(
+        expect.anything(),
+        expect.stringContaining("something")
+      );
+      expect(spy).toHaveBeenCalledWith(expect.stringContaining("something"));
+    });
+  });
+});
